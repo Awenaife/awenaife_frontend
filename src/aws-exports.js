f007ff8828680bc56ecbc76dbcb7d34228eea463
Jean Silva <jean-vs@hotmail.com>
2025-01-23 10:48:56 -0300

    Jean

---
+++ src/aws-exports.js
@@ -0,0 +1,29 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_identity_pool_id": "us-east-1:5973a087-cdd3-4e8f-8ce3-fa43e6193df3",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_XmiCxBvR7",
+    "aws_user_pools_web_client_id": "3h6kke9bv60l6ghe580nmgaqdk",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;
