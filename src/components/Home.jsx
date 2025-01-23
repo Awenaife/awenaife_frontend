f1360f45f3a092b489e80e55f531c5c47ef7d537
Jean Silva <jean-vs@hotmail.com>
2025-01-23 19:31:39 -0300

    jean

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
 import PropTypes from "prop-types";
 import { signOut, deleteUser } from "aws-amplify/auth";
+import { useState } from "react";
+import Chat from "./Chat";
+import './Home.css';
 
 function Home({ onLogout }) {
+  const [showChat, setShowChat] = useState(false);
+  const [isHoveredC, setIsHoveredC] = useState(false);
+  const [isHoveredE, setIsHoveredE] = useState(false);
   const handleSignOut = async () => {
     try {
       await signOut();
@@ -24,16 +30,33 @@ function Home({ onLogout }) {
     }
   };
 
+  if (showChat) {
+    return <Chat onBack={() => setShowChat(false)} />;
+  }
+
   return (
     <div className="main-container">
       <h2>Bem-vindo AWENAIFE!</h2>
+      <button
+        onClick={() => setShowChat(true)}
+        style={{
+          backgroundColor: isHoveredC ? "#218838" : "#28a745",
+          marginTop: "1rem",
+        }}
+        onMouseEnter={() => setIsHoveredC(true)}
+        onMouseLeave={() => setIsHoveredC(false)}
+      >
+        Chat
+      </button>
       <button onClick={handleSignOut}>Sair</button>
       <button
         onClick={handleDeleteAccount}
         style={{
-          backgroundColor: "#e74c3c",
+          backgroundColor: isHoveredE ? "#8b0000" : "#e74c3c",
           marginTop: "1rem",
         }}
+        onMouseEnter={() => setIsHoveredE(true)}
+        onMouseLeave={() => setIsHoveredE(false)}
       >
         Excluir minha conta
       </button>
